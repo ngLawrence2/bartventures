2556dc5ff9f51e12cb8377143c025a9e3f29af67
Jing <jingxia1219@gmail.com>
2018-08-25 18:32:06 -0700

    fix mongoose

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const express = require("express");
-const router = express.Router();
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const events = require('./routes/api/events');
+const bodyParser = require('body-parser');
 
 mongoose 
     .connect(db)
@@ -10,8 +12,13 @@ mongoose
     .catch( err => console.log(err));
 
 app.get("/", (req, res) => res.send(" Bart App for Tourists"));
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`)); 
- 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/events", events);
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
