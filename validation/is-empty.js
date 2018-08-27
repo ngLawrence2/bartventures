89d9de6d2376266cdc0e08ed531f79fece3f2d92
Jing <jingxia1219@gmail.com>
2018-08-27 10:28:58 -0700

    fixed validation, able to register new user now on postmna

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,5 +1,5 @@
 const isEmpty = value => 
-    value === undefinied ||
+    value === undefined ||
     value === null || 
     (typeof value === 'object' && Object.keys(value).length === 0 ) ||
     (typeof value === 'string' && value.trim().length === 0);
