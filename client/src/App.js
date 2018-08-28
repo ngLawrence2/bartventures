1b4a6e6b9b8e02ccda52448fd217253cff45e012
Jing <jingxia1219@gmail.com>
2018-08-27 19:58:58 -0700

    added googlemap.html

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state= {
+    data: null
+  }
+  componentDidMount(){
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express}))
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if(response.status != 200) {
+      throw Error(body.message)
+    }
+    return body;
+  }
   render() {
     return (
       <div className="App">
