3f19776168d5a6f2eb81f749534fb86b1098d9da
Jing <jingxia1219@gmail.com>
2018-08-29 09:49:21 -0700

    add

---
+++ frontend/src/components/navBar/nav_bar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../../style/test.css';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayCorrectSessionButton=this.displayCorrectSessionButton.bind(this);
+  }
+
+  displayCorrectSessionButton() {
+    if(this.props.currentUser===undefined) {
+      return (
+        <div className="sessionLinks">
+          <Link to = "/login">Log in!</Link>
+          <Link to = "/signup">Sign Up!</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Hello {this.props.currentUser}</h1>
+            <button onClick= {this.props.logoutUser}>Log out</button>
+        </div>
+      );
+    }
+  }
+
+
+  render() {
+    const displayLinks = this.displayCorrectSessionButton();
+    return (
+      <div>
+        <h1>Bart Ventures</h1>
+        {displayLinks}
+
+      </div>
+    );
+  }
+}
+
+export default NavBar;
--- frontend/src/components/session_form/session_form.jsx
+++ frontend/src/components/session_form/session_form.jsx
@@ -10,8 +10,16 @@ class SessionForm extends React.Component {
       password2: '',
       name:''
     };
+    if(this.props.formType === 'login') {
+      this.state = {
+        email: '',
+        password:''
+      };
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.update=this.update.bind(this);
+    this.displaySignUpForms = this.displaySignUpForms.bind(this);
+    this.displayErrors=this.displayErrors.bind(this);
   }
 
   update(e,field) {
@@ -21,21 +29,51 @@ class SessionForm extends React.Component {
     });
   }
 
+  componentWillUnmount() {
+    this.props.clearErrors();
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     const user = Object.assign({}, this.state);
     this.props.processForm(user);
   }
 
+  displaySignUpForms() {
+    if(this.props.formType === 'signup') {
+      return (
+        <div>
+          <label>Enter Name:
+              <input type="text" value = {this.state.name} onChange={(e)=>this.update(e,"name")} />
+          </label>
+          <label>
+              Repeat Password:
+              <input type="text" value = {this.state.password2} onChange={(e)=>this.update(e,"password2")} />
+          </label>
+        </div>
+      );
+    }
+  }
+
+  displayErrors() {
+    return (
+      <div>
+        {this.props.errors.map( (err,idx) => {
+           return (<ul key={idx}>{err}</ul>)
+        })}
+      </div>
+    );
+  }
+
+
 
   render() {
+    const display = this.displaySignUpForms();
+    const error = this.displayErrors();
     return (
       <div>
         <h2>Welcome to BartVenture</h2>
         <form onSubmit={this.handleSubmit}>
-        <label>Enter Name:
-            <input type="text" value = {this.state.name} onChange={(e)=>this.update(e,"name")} />
-        </label>
         <label>
             Enter Email:
             <input type="text" value = {this.state.email} onChange={(e)=>this.update(e,"email")}/>
@@ -44,15 +82,15 @@ class SessionForm extends React.Component {
             Enter Password:
             <input type="password"  value = {this.state.password} onChange={(e)=>this.update(e,"password")} />
         </label>
-        <label>
-            Repeat Password:
-            <input type="password" value = {this.state.password2} onChange={(e)=>this.update(e,"password2")} />
-        </label>
-        <input type="submit" value="Create User" />
+          {display}
+        <input type="submit" value={this.props.buttonText} />
         </form>
-      </div>
+        {this.props.navLink}
+        {error}
 
-    );
+
+      </div>
+    )
   }
 }
 
