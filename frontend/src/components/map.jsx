7b4cfb08a31d676ce2c7d39e2ee792ad83004931
Jing <jingxia1219@gmail.com>
2018-08-29 21:21:32 -0700

    add border to map

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -3,16 +3,18 @@ import { withRouter } from 'react-router-dom';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
 /*global google*/
 const mapOptions = {
+
     center: {
         lat: 37.773972,
         lng: -122.431297
     },
     zoom: 13
-}; 
+};
+
 
 class BartMap extends React.Component {
     constructor(props) {
-    super(props) 
+        super(props)
         this.state = {
             center: {
                 lat: 37.773972,
@@ -29,26 +31,28 @@ class BartMap extends React.Component {
     //     })
     //     );
     // }
-    
+
     render() {
         const GoogleMapExample = withGoogleMap(props => (
-            <GoogleMap 
-            defaultCenter = { { lat: 37.773972, lng: -122.431297 }}
-            defaultZoom = { 11 }
+            <GoogleMap
+                defaultCenter={{ lat: 37.801239, lng: -122.258301 }}
+                defaultZoom={10}
             >
             </ GoogleMap>
         ))
         return (
-           <div className="map" ref="map">
-            {/* Is Map Showing */}
-        <GoogleMapExample  
-        containerElement={ <div style={{ height: `980px`, width: 
-    '750px'}}/> }
-    mapElement={ <div style={{height: `100%`}} />}
-        />
-            </div> 
+            <div className="map" ref="map">
+                {/* Is Map Showing */}
+                <GoogleMapExample
+                    containerElement={<div style={{
+                        height: `750px`, width:
+                            '750px'
+                    }} />}
+                    mapElement={<div style={{ height: `100%`, border: `1px solid black` }} />}
+                />
+            </div>
 
-        
+
         );
     }
 }
