26e08a38951c06fc37060b55fc5924b32b09f04a
“Peter <peterwongisnow@gmail.com>
2018-08-29 23:31:48 -0700

    Remove sign up butto from login page

--- routes/api/search.js
+++ routes/api/search.js
@@ -6,37 +6,14 @@ const router =  express.Router();
 
 router.get("/:budget/:loc", (req,res) => {
   let stations = [];
-<<<<<<< HEAD
-  Request.get("https://api.bart.gov/api/stn.aspx?cmd=stns&key=QMBS-5LIW-9J2T-DWE9&json=y", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    // console.log(body["root"]);
-    // console.log(body);
-    // console.log(Object.keys(body));
-     // res.send(body["root"]);
-     let keys = Object.keys(body);
-     let data = JSON.parse(body);
-
-=======
   axios.get("https://api.bart.gov/api/stn.aspx?cmd=stns&key=QMBS-5LIW-9J2T-DWE9&json=y").then((response) => {
      let keys = Object.keys(response);
      let data = response.data;
->>>>>>> d957a4a485ebf12f773717e8b6b4253cd7c70e6f
      for(let i = 0 ; i < 48 ; i++) {
        let stationObj = {
          name: data.root.stations.station[i].name,
          abbr: data.root.stations.station[i].abbr,
          lat: data.root.stations.station[i].gtfs_latitude,
-<<<<<<< HEAD
-         lon: data.root.stations.station[i].gtfs_longitude,
-       };
-       stations.push(stationObj);
-     }
-    res.send(stations);
-  });
-  // console.log(stations.slice(30));
-=======
          lng: data.root.stations.station[i].gtfs_longitude
        };
        stations.push(stationObj);
@@ -67,7 +44,6 @@ router.get("/:budget/:loc", (req,res) => {
       console.log(err);
     })
 
->>>>>>> d957a4a485ebf12f773717e8b6b4253cd7c70e6f
 
 });
 
