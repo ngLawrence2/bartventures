0beda5d4d2ce77f4cbbf4d9c2e932ae77d843373
“Peter <peterwongisnow@gmail.com>
2018-08-31 13:21:26 -0700

    Add section for Attractions

--- frontend/src/components/attractions/attractions.jsx
+++ frontend/src/components/attractions/attractions.jsx
@@ -11,12 +11,31 @@ class Attractions extends React.Component {
 
     return (
       <div className="attractions-body">
+          <div className="attractions-header">
           Places to Explore
+          </div>
           <div>
             <ul className="test-list">
-              <li>Attraction1 Title here</li>
-              <li>Attraction2 Title here</li>
+              <li> Attraction1 Photo and Attraction1 Title here</li>
+                <p className="attractions-desc">Description1 here</p>
+              <li>Attraction2 Photo and Title here</li>
+                <p className="attractions-desc">Description2 here</p>
+              <li>Attraction3 Photo and Title here</li>
+                <p className="attractions-desc">Description3 here</p>
               <li>Attraction3 Title here</li>
+                <p className="attractions-desc">Description4 here</p>
+              <li>Attraction3 Title here</li>
+                <p className="attractions-desc">Description5 here</p>
+              <li>Attraction3 Title here</li>
+                <p className="attractions-desc">Description6 here</p>
+              <li>Attraction3 Title here</li>
+                <p className="attractions-desc">Description7 here</p>
+              <li>Attraction3 Title here</li>
+                <p className="attractions-desc">Description8 here</p>
+              <li>Attraction3 Title here</li>
+                <p className="attractions-desc">Description9 here</p>
+              <li>Attraction3 Title here</li>
+                <p className="attractions-desc">Description10 here</p>
             </ul>
           </div>
       </div>
