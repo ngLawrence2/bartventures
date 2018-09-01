06475c68d1c3c7851e15ac3ef41fb0a7c6c8bd20
urosh <urosh.stojkovikj@gmail.com>
2018-09-01 15:04:23 -0700

    added some attractions with images in seeds.jsx

--- frontend/src/components/attractions/attractions.jsx
+++ frontend/src/components/attractions/attractions.jsx
@@ -20,24 +20,24 @@ class Attractions extends React.Component {
               <p className="attractions-desc">{attractions[4].name}</p>
               <li><img src={ attractions[4].image } height="400" width="600" /></li>
                 <p className="attractions-desc">{attractions[4].description}</p>
-              <li>Attraction2 Photo and Title here</li>
-                <p className="attractions-desc">Description2 here</p>
-              <li>Attraction3 Photo and Title here</li>
-                <p className="attractions-desc">Description3 here</p>
-              <li>Attraction3 Title here</li>
-                <p className="attractions-desc">Description4 here</p>
-              <li>Attraction3 Title here</li>
-                <p className="attractions-desc">Description5 here</p>
-              <li>Attraction3 Title here</li>
-                <p className="attractions-desc">Description6 here</p>
-              <li>Attraction3 Title here</li>
-                <p className="attractions-desc">Description7 here</p>
-              <li>Attraction3 Title here</li>
-                <p className="attractions-desc">Description8 here</p>
-              <li>Attraction3 Title here</li>
-                <p className="attractions-desc">Description9 here</p>
-              <li>Attraction3 Title here</li>
-                <p className="attractions-desc">Description10 here</p>
+              // <li>Attraction2 Photo and Title here</li>
+              //   <p className="attractions-desc">Description2 here</p>
+              // <li>Attraction3 Photo and Title here</li>
+              //   <p className="attractions-desc">Description3 here</p>
+              // <li>Attraction3 Title here</li>
+              //   <p className="attractions-desc">Description4 here</p>
+              // <li>Attraction3 Title here</li>
+              //   <p className="attractions-desc">Description5 here</p>
+              // <li>Attraction3 Title here</li>
+              //   <p className="attractions-desc">Description6 here</p>
+              // <li>Attraction3 Title here</li>
+              //   <p className="attractions-desc">Description7 here</p>
+              // <li>Attraction3 Title here</li>
+              //   <p className="attractions-desc">Description8 here</p>
+              // <li>Attraction3 Title here</li>
+              //   <p className="attractions-desc">Description9 here</p>
+              // <li>Attraction3 Title here</li>
+              //   <p className="attractions-desc">Description10 here</p>
             </ul>
           </div>
       </div>
