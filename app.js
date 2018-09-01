494537ad02e03c58643da645dcf35c6f7dd97dd6
Jing <jingxia1219@gmail.com>
2018-08-31 18:01:45 -0700

    heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
@@ -8,6 +9,14 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const search = require('./routes/api/search');
 require('./config/passport')(passport);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 mongoose
     .connect(db, {useNewUrlParser: true })
