4f60d53bf6c18cebbc7679dd0fe9b8fbf39f6456
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-03 12:37:29 -0700

    change path resolve

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ require('./config/passport')(passport);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
 
