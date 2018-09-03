2f8a4b5daf04693405e6350ae9b1fd8672575906
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-03 08:42:39 -0700

    keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb://jingxia1219:jinjin1219@ds133262.mlab.com:33262/bart-guide-for-tourists",
-  //Make sure this is your own unique string
-  secretOrKey: "secret"
-};
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+}
